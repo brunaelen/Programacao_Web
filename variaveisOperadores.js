a898b8be7fde5a3373d9bd75e439dfdb552c7606
brunaelen <brunaelen9152@gmail.com>
2024-03-13 15:09:52 -0300

    first commit

---
+++ variaveisOperadores.js
@@ -0,0 +1,18 @@
+const n1 = 10;
+const n2 = 90;
+
+let soma = n1 + n2;
+
+console.log(`Resultado da soma entre ${n1} e ${n2} é ${soma}`);
+
+let sub = n1 - n2;
+
+console.log(`Resultado da subtração entre ${n1} e ${n2} é ${sub}`);
+
+let multi = n1 * n2;
+
+console.log(`Resultado da multiplicação entre ${n1} e ${n2} é ${multi}`);
+
+let divi = n1 / n2;
+
+console.log(`Resultado da divisão entre ${n1} e ${n2} é ${divi}`);
